06a2f8f26233f1d8b086bc6e163e1cad886c1649
SIVAJI22 <sivaji0722@gmail.com>
2023-05-25 13:22:45 +0530

    inital commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import bootstrap from 'bootstrap/dist/js/bootstrap'
+import Kommunicate from '@kommunicate/kommunicate-chatbot-plugin';
+  
 
+Kommunicate.init("21424902ab81ff5f3ddef733debb69792" ,{"popupWidget":true,"automaticChatOpenOnNavigation":true})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
